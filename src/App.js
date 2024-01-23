bc1e085873018e8cfe5fd1a4d5b3708d87fdbfed
김애리 <zjadofl0408@gmail.com>
2024-01-23 23:10:07 +0900

    feat: Todo 삭제 기능 구현

--- src/App.js
+++ src/App.js
@@ -8,14 +8,10 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items : [
-        {id:"0", title:"Hello World 1", done:true},
-        {id:"1", title:"Hello World 2", done:false},
-      ],
+      items : [],
     };
   }
 
-  // (1) 함수 추가
   add = (item) => {
     const thisItems = this.state.items;
     item.id = "ID-" + thisItems.length; //key를 위한 id 추가
@@ -25,18 +21,27 @@ class App extends React.Component {
     console.log("items: ", this.state.items.items);
   }
 
+  delete = (item) => {
+    const thisItems = this.state.items;
+    console.log("Before Update Items : ", this.state.items);
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({items:newItems}, () => {
+      //디버깅 콜백
+      console.log("Update Items : ", this.state.items)
+    });
+  }
+
   render() {
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{margin:16}}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id}/>
+            <Todo item={item} key={item.id} delete={this.delete}/>
           ))}
         </List>
       </Paper>
     );
 
-    // (2) 함수 연결
     return (
       <div className="App">
         <Container maxWidth="md">
--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import {ListItem, ListItemText, InputBase, Checkbox} from "@material-ui/core";
+import {ListItem, ListItemText, InputBase, Checkbox,
+    ListItemSecondaryAction, IconButton
+} from "@material-ui/core";
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import { DeleteOutlined } from '@material-ui/icons';
 
 class Todo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {item : props.item};
+        this.delete = props.delete;
+    }
+
+    deleteEventHandler = () => {
+        this.delete(this.state.item)
     }
 
     render() {
@@ -16,13 +25,21 @@ class Todo extends React.Component {
                     <InputBase
                         inputProps={{"aria-label":"naked"}}
                         type="text"
-                        id={item.id}
-                        name={item.id}
+                        id={item.id} // 각 리스트를 구분하려고 id를 연결
+                        name={item.id} // 각 리스트를 구분하려고 id를 연결
                         value={item.title}
                         multiline={true}
                         fullWidth={true}
                     />
                 </ListItemText>
+                
+                <ListItemSecondaryAction>
+                    <IconButton 
+                        aria-label="Delete Todo"
+                        onClick={this.deleteEventHandler}>
+                        <DeleteOutlined />
+                    </IconButton>
+                </ListItemSecondaryAction>
             </ListItem>
         );
     }
